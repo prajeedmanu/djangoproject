5fc78f2c1f5a081ea1a9afbdcc6d59d861827c48
unknown <prajeed19@gmail.com>
2021-11-29 16:18:00 +0530

    inmakes

---
+++ todoproject/todoapp/urls.py
@@ -0,0 +1,13 @@
+from . import views
+from django.urls import path
+
+
+urlpatterns = [
+    path('',views.todo,name='todo'),
+    path('delete/<int:id>/', views.delete, name='delete'),
+    path('update/<int:id>/', views.update, name='update'),
+    path('cbvhome', views.tasklistview.as_view(), name='cbvhome'),
+    path('cbvdetail/<int:pk>/',views.taskdetailview.as_view(),name='cbvdetail'),
+    path('cbvupdate/<int:pk>/',views.taskupdateview.as_view(),name='cbvupdate'),
+    path('cbvdelete/<int:pk>/',views.taskdeleteview.as_view(),name='cbvdelete'),
+    ]
